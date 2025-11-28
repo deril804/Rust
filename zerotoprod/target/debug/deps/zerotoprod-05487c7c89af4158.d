aad192ad8e9a8aa462d8c7d0f09793405e560eac
deril804 <derillabi@gmail.com>
2025-11-29 00:09:21 +0530

    modify the folder structure

---
+++ zerotoprod/target/debug/deps/zerotoprod-05487c7c89af4158.d
@@ -0,0 +1,5 @@
+/Users/derilxavier/Documents/rust-learning/zerotoprod/zerotoprod/target/debug/deps/zerotoprod-05487c7c89af4158.d: src/main.rs
+
+/Users/derilxavier/Documents/rust-learning/zerotoprod/zerotoprod/target/debug/deps/zerotoprod-05487c7c89af4158: src/main.rs
+
+src/main.rs:
---
+++ zerotoprod/target/debug/zerotoprod.d
@@ -0,0 +1,1 @@
+/Users/derilxavier/Documents/rust-learning/zerotoprod/zerotoprod/target/debug/zerotoprod: /Users/derilxavier/Documents/rust-learning/zerotoprod/zerotoprod/src/main.rs
